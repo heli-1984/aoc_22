824321928ba220dfb7c70921e9d3043a5e2ce8c4
heli-1984 <lukewarm.protozoam@posteo.eu>
2022-12-08 22:26:30 +0200

    more "progress"

--- 8/rust_8/src/main.rs
+++ 8/rust_8/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let data = include_str!("test.txt");
+    let data = include_str!("input.txt");
     let length = data.lines().next().unwrap().chars().count();
     let height = data.lines().count();
     let mut trees = Vec::new();
@@ -19,14 +19,17 @@ fn main() {
             let mut temp_vec2:Vec<usize> = [part[0]].to_vec(); 
             for part2 in part.clone() {
                 let mut index = Vec::new();
-                if &part2 > temp_vec2.last().unwrap() && part2 != *part.first().unwrap() && part2 != *part.last().unwrap() {
-                    index.push(index_h);
-                    index.push(index_l);
-                    //println!("{:?}", index);
-                    visible.push(index.clone());
-                }
-                temp_vec2.push(part2);
-                temp_vec2.sort();
+                if part2 != *part.first().unwrap() && part2 != *part.last().unwrap() {
+                    if &part2 > temp_vec2.last().unwrap() {
+                        index.push(index_h);
+                        index.push(index_l);
+                        //println!("tempvec2: {:?}, current: {:?}", temp_vec2 ,part2);
+                        //println!("{:?}", index);
+                        visible.push(index.clone());
+                    }
+                    temp_vec2.push(part2);
+                    temp_vec2.sort();
+            }
                 index_l += 1;
             }
         }
@@ -40,16 +43,19 @@ fn main() {
         let mut index_l = 0;
         if part != *trees.first().unwrap() && part != *trees.last().unwrap() {
             let mut temp_vec2:Vec<usize> = [part[length - 1]].to_vec();
-            for part2 in part_rev {
+            for part2 in part_rev.clone() {
                 let mut index = Vec::new();
-                if &part2 > temp_vec2.last().unwrap() && part2 != *part.first().unwrap() && part2 != *part.last().unwrap() {
-                    index.push(index_h);
-                    index.push(index_l);
-                    //println!("{:?}", index);
-                    visible.push(index.clone());
+                if part2 != *part_rev.first().unwrap() && part2 != *part_rev.last().unwrap() {
+                    if &part2 > temp_vec2.last().unwrap()  {
+                        index.push(index_h);
+                        index.push(index_l);
+                        //println!("tempvec2: {:?}, current: {:?}", temp_vec2 ,part2);
+                        //println!("{:?}", index);
+                        visible.push(index.clone());
+                    }
+                    temp_vec2.push(part2);
+                    temp_vec2.sort();
                 }
-                temp_vec2.push(part2);
-                temp_vec2.sort();
                 index_l += 1;
             }
         }
@@ -57,7 +63,7 @@ fn main() {
     }
 
     let mut index_l = 0;
-    for i in 0..length - 1 {
+    for i in 0..(length - 1) {
         let mut index_h = 1;
         if i != 0 {
             let mut temp_vec2:Vec<usize> = [trees[0][i]].to_vec();
@@ -66,6 +72,8 @@ fn main() {
                 if trees[i][n] > *temp_vec2.last().unwrap() && trees[i][n] != trees[i][0] && trees[i][n] != trees[i][length - 1] {
                     index.push(index_h);
                     index.push(index_l);
+                    //println!("{i}, {n}");
+                    //println!("vec: {:?}, cur: {}", temp_vec2, trees[n][i]);
                     //println!("{:?}", index);
                     visible.push(index);
                 }
@@ -77,31 +85,31 @@ fn main() {
         index_l += 1;
     }
     let mut index_l = 0;
-    for i in 0..(length - 1) {
-        let mut index_h = 0;
+    trees.reverse();
+    for i in 0..length - 1 {
+        let mut index_h = 1;
         if i != 0 {
-            let mut temp_vec2:Vec<usize> = [trees[height - 1][i]].to_vec();
-            for n in (1..height).rev() {
+            let mut temp_vec2:Vec<usize> = [trees[0][i]].to_vec();
+            for n in 1..height {
                 let mut index = Vec::new();
-                if trees[i][n] > *temp_vec2.last().unwrap() {
-                    index.push(index_h);
+                if trees[n][i] > *temp_vec2.last().unwrap() && trees[i][n] != trees[i][0] && trees[i][n] != trees[i][length - 1] {
+                    index.push(height - 1 - index_h);
                     index.push(index_l);
-                    println!("tempvec2: {:?}", temp_vec2);
-                    println!("{:?}", index);
-                    println!("{i}, {n}");
+                    //println!("{i}, {n}");
+                    //println!("tempvec2: {:?}, current: {:?}", temp_vec2 ,trees[n][i]);
+                    //println!("{:?}", index);
                     visible.push(index);
                 }
-                temp_vec2.push(trees[i][n]);
+                temp_vec2.push(trees[n][i]);
                 temp_vec2.sort();
                 index_h += 1;
             }
         }
         index_l += 1;
     }
-
     visible.sort();
     visible.dedup();
     // calculate total visible trees, because all the trees on the outside are visible:
-    let total = visible.len() + 2 * length + 2 * (height - 2);
-    print!("Total visible trees: {}", total);
+    let total = visible.len() + 2 * height + 2 * (length - 2);
+    println!("Total visible trees: {}", total);
 }
