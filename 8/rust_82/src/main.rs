6334c566c7b0672c64517c21ea90aaf72a28fb83
heli-1984 <lukewarm.protozoam@posteo.eu>
2022-12-09 10:48:54 +0200

    cleanup and rustfmt

--- 8/rust_82/src/main.rs
+++ 8/rust_82/src/main.rs
@@ -13,18 +13,15 @@ fn main() {
     let mut visible;
     let mut current_tree = 0;
     println!("{}", data);
-    let mut scenic_score = vec![0; length*height];
+    let mut scenic_score = vec![0; length * height];
     //left to right check
     for row in trees.clone() {
         for column in 0..length {
             current_tree += 1;
-            let mut temp_vec = Vec::new();
             visible = 0;
-            //print!("{} ", row[column]);
             for test in column + 1..length {
-                temp_vec.push(row[test]);
                 if row[test] < row[column] {
-                    if *temp_vec.first().unwrap() != 0 {
+                    if row[test] != 0 {
                         visible += 1;
                     }
                 } else {
@@ -33,8 +30,6 @@ fn main() {
                 }
             }
             scenic_score[current_tree - 1] = visible;
-            //println!("{:?}", temp_vec);
-            //println!("visible: {}", visible);
         }
     }
     println!("{:?}", scenic_score);
@@ -45,23 +40,18 @@ fn main() {
         for column in 0..length {
             current_tree += 1;
             visible = 0;
-            //let mut temp_vec = Vec::new();
-            //print!("row[column]: {} ", row[column]);
             for test in (0..column).rev() {
-                //print!("row[test]: {} ", row[test]);
                 if row[test] < row[column] {
                     if row[test] != 0 {
                         visible += 1;
                     }
-                    } else {
-                        visible += 1;
-                        break;
-                    }
+                } else {
+                    visible += 1;
+                    break;
                 }
-                //println!("visible: {visible}");
-                scenic_score[current_tree - 1] *= visible;
             }
+            scenic_score[current_tree - 1] *= visible;
         }
-    //println!("{:?}", trees_seen);
+    }
     println!("{:?}", scenic_score);
 }
